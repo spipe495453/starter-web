3d093aae667a3e531438e526682ed3a88597039b
Soeren Eschweiler <soeren.eschweiler@rwth-aachen.de>
2020-03-24 20:31:24 +0100

    Further modified newfile.py

--- newfile.py
+++ newfile.py
@@ -1,6 +1,9 @@
 #%%
 import json
-data = {'A':[1,5,6,8],'B':[4,7,9,6],'D':[4,7,45,9]}
+data = {'ABCD':[1,5,6,8],'ABA':[4,7,9,6],'D':[4,7,45,9]}
 json_temp = json.dumps(data)        
+
+# %%
+json_temp.count('AB')
 
 # %%
