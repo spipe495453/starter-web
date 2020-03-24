7fd865a8fa066cda6bdab8c01e66d1e64ebfd93b
Soeren Eschweiler <soeren.eschweiler@rwth-aachen.de>
2020-03-24 20:33:59 +0100

    More Changes to newfile.py

--- newfile.py
+++ newfile.py
@@ -1,6 +1,8 @@
 #%%
 import json
 data = {'ABCD':[1,5,6,8],'ABA':[4,7,9,6],'D':[4,7,45,9]}
+data_2 = {'ABCD':[1,5,6,8],'ABA':[4,7,9,6],'D':[4,7,45,9]}
+data_3 = {'ABCD':[1,5,6,8],'ABA':[4,7,9,6],'D':[4,7,45,9]}
 json_temp = json.dumps(data)        
 
 # %%
